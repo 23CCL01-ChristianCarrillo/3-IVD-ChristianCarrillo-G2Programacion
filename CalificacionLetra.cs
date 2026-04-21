b91776f463938879d1e7c3dd7958de58866e89dc
23CCL01-ChristianCarrillo <ccarrillodb23@gmail.com>
2026-04-21 17:11:57 -0600

    Add files via upload

---
+++ CalificacionLetra.cs
@@ -0,0 +1,33 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese una letra (A, B, C, D, F): ");
+        char letra = Char.ToUpper(Console.ReadKey().KeyChar);
+        Console.WriteLine();
+
+        switch (letra)
+        {
+            case 'A':
+                Console.WriteLine("Excelente");
+                break;
+            case 'B':
+                Console.WriteLine("Bueno");
+                break;
+            case 'C':
+                Console.WriteLine("Regular");
+                break;
+            case 'D':
+                Console.WriteLine("Deficiente");
+                break;
+            case 'F':
+                Console.WriteLine("Reprobado");
+                break;
+            default:
+                Console.WriteLine("Letra inválida");
+                break;
+        }
+    }
+}
---
+++ CalificacionLetra.psc
@@ -0,0 +1,21 @@
+Algoritmo CalificacionLetra
+    Definir letra Como Caracter
+    
+    Escribir "Ingrese una letra (A, B, C, D, F):"
+    Leer letra
+    
+    Segun letra Hacer
+        "A":
+            Escribir "Excelente"
+        "B":
+            Escribir "Bueno"
+        "C":
+            Escribir "Regular"
+        "D":
+            Escribir "Deficiente"
+        "F":
+            Escribir "Reprobado"
+        De Otro Modo:
+            Escribir "Letra inválida"
+    FinSegun
+FinAlgoritmo
