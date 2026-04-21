bf8d7283b16ebe5446c7c489a2edc7ba94b481dd
23CCL01-ChristianCarrillo <ccarrillodb23@gmail.com>
2026-04-21 17:19:45 -0600

    Add files via upload

---
+++ MenuComida.cs
@@ -0,0 +1,35 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("----- MENÚ -----");
+        Console.WriteLine("1 → Hamburguesa");
+        Console.WriteLine("2 → Pizza");
+        Console.WriteLine("3 → Pollo frito");
+        Console.WriteLine("4 → Ensalada");
+        Console.Write("Seleccione una opción: ");
+
+        int opcion = int.Parse(Console.ReadLine());
+
+        switch (opcion)
+        {
+            case 1:
+                Console.WriteLine("Usted pidió: Hamburguesa");
+                break;
+            case 2:
+                Console.WriteLine("Usted pidió: Pizza");
+                break;
+            case 3:
+                Console.WriteLine("Usted pidió: Pollo frito");
+                break;
+            case 4:
+                Console.WriteLine("Usted pidió: Ensalada");
+                break;
+            default:
+                Console.WriteLine("Opción inválida");
+                break;
+        }
+    }
+}
